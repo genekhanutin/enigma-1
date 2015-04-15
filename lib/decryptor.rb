25bdc8b014dbfb8de9ce8a7d28e2cdd31f8eafff
genekhanutin <genekhanutin@gmail.com>
2015-04-14 19:20:04 -0600

    decryptor tests passing

---
+++ lib/decryptor.rb
@@ -0,0 +1,39 @@
+require_relative 'rotator'
+
+class Decryptor
+  attr_reader :rotator, :message
+
+  def initialize(message = "", rotator = Rotator.new)
+    @rotator  = rotator
+    @message  = message
+  end
+
+  def decrypt_message
+    decrypt_value.map do |num|
+      rotator.character_map[num]
+    end.join
+  end
+
+  def decrypt_value
+    message.chars.each_slice(4).map do |slice|
+      slice.map.with_index do |letter, index|
+        value_on_character_map(letter,index)
+      end
+    end.flatten
+  end
+
+  def value_on_character_map(letter, index)
+    (rotator.character_map.index(letter) - rotator.rotations[index]) % 39
+  end
+end
+# instantiate new instance of rotator class
+# use its rotation methods and character map to do encryption
+
+# find index position of message-character on character-map (.index())
+# .index will return index position of letter from the message on character map
+# add rotation vlaue to that index position
+# find new value at that position
+
+
+# message will be passed into encryptor
+# then encryptor will be passed/instantiated in encrypt
---
+++ test/decryptor_test.rb
@@ -0,0 +1,59 @@
+require 'minitest'
+require 'minitest/autorun'
+require './lib/decryptor'
+
+class DecryptorTest < Minitest::Test
+  attr_reader :rotator
+
+  def setup
+    test_key  = [4,1,5,1,2]
+    test_date = Date.new('020315').last_four_of_squared_date
+    @rotator = Rotator.new(test_key, test_date)
+  end
+
+  def test_decryptor_exists
+    assert Decryptor.new
+  end
+
+  def test_it_decrypts_one_letter
+    decryptor = Decryptor.new('s', rotator)
+
+    assert_equal 'h', decryptor.decrypt_message
+  end
+
+  def test_it_encrypts_two_letters
+    decryptor = Decryptor.new('sv', rotator)
+
+    assert_equal 'he', decryptor.decrypt_message
+  end
+
+  def test_it_decrypts_three_letters
+    decryptor = Decryptor.new('svz', rotator)
+
+    assert_equal 'hel', decryptor.decrypt_message
+  end
+
+  def test_it_decrypts_four_letters
+    decryptor = Decryptor.new('svz2', rotator)
+
+    assert_equal 'hell', decryptor.decrypt_message
+  end
+
+  def test_it_decrypts_five_letter_word
+    decryptor = Decryptor.new('svz2z', rotator)
+
+    assert_equal 'hello', decryptor.decrypt_message
+  end
+
+  def test_it_decrypts_phrase_with_space
+    decryptor = Decryptor.new('svz2zo 522r', rotator)
+
+    assert_equal 'hello world', decryptor.decrypt_message
+  end
+
+  def test_it_decrypts_phrase_with_spaces_and_number
+    decryptor = Decryptor.new('svz2zo 522ro,', rotator)
+
+    assert_equal 'hello world 1', decryptor.decrypt_message
+  end
+end
